488908e90dde78c01bddc5f6fd2aa3da48f31376
Tomek Wiszniewski <t.wiszniewski@gmail.com>
2015-06-11 11:13:54 +0200

    Satisfy the spec

---
+++ module/index.js
@@ -0,0 +1,9 @@
+const fold = require('1-liners/fold');
+
+export default (input) => ({
+  output: fold((output, chunk) => (
+    chunk.output == null ?
+    output :
+    output + chunk.output
+  ), '', input.chunks)
+});
