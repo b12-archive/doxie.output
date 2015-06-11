8bab2fd734cac141e82a50156ff541a5751f6fab
Tomek Wiszniewski <t.wiszniewski@gmail.com>
2015-06-11 12:30:35 +0200

    Implement exception handling

--- module/index.js
+++ module/index.js
@@ -1,12 +1,38 @@
 const fold = require('1-liners/fold');
+const assign = require('object-assign');
+const {freeze} = Object;
 
-export default (input) => ({
-  output: fold((output, chunk) => (
+const warning = (warning) => `[doxie --output] Warning: ${warning}\n`;
+
+export default (input) => {
+  // Determine the output.
+  const output = fold((output, chunk) => (
     chunk.output == null ?
     output :
     output + chunk.output
-  ), '', input.chunks),
+  ), '', input.chunks);
 
-  version: input.version,
-  chunks: input.chunks,
-});
+  // Construct the error string:
+  const errorMessages = [];
+
+  // * Check the output
+  if (!output) errorMessages.push(warning(
+    'Output is empty. Perhaps you’ve forgotten to render your comments? ' +
+    '<http://npm.im/doxie.template> might put you on the right track.'
+  ));
+
+  // * Check the version
+  const {version} = input;
+  if (typeof version !== 'number') errorMessages.push(warning(
+    'Unable to determine the version of input data. The results may be ' +
+    'unexpected.'
+  )); else if (version !== 1) errorMessages.push(warning(
+    'Incompatible version of input data. The results may be unexpected.'
+  ));
+
+  // Pass data down the pipeline.
+  return freeze(assign({}, input, {
+    output,
+    error: errorMessages.join() || null,
+  }));
+};
